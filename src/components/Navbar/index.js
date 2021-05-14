9838e25ab8518699dfffa9d79732a18889c0a16d
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-05-14 21:55:57 +0200

    Add navbar component and style file

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import { Nav, NavContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElmenets';
+
+const Navbar = ({ toggle: sideBarToggle }) => {
+    
+    return (
+        <Nav>
+            <NavContainer>
+                <NavLogo>
+                    Smoothie Delight
+                </NavLogo>
+                <MobileIcon onClick={sideBarToggle}>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to="about" smooth={true} duration={500} spy={true} exact='true' offset={-80}>About</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="health" smooth={true} duration={500} spy={true} exact='true' offset={-80}>Health</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="experience" smooth={true} duration={500} spy={true} exact='true' offset={-80}>Experience</NavLinks>
+                    </NavItem>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to="newsletter" smooth={true} duration={500} spy={true} exact='true' offset={-80}>Sign up</NavBtnLink>
+                </NavBtn>
+            </NavContainer>
+            
+        </Nav>
+    );
+};
+
+export default Navbar;
