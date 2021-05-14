8451bc98391058b2ec92f73a3b345de0bc260ee8
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-05-14 21:57:01 +0200

    Add home page
    Home page is a component which wrapped all home elements

---
+++ src/pages/home.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Navbar/Sidebar';
+
+const Home = () => {
+    
+    const [ isOpen, setIsOpen ] = useState(false);
+
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <>
+            <Navbar toggle={setIsOpen} />
+            <Sidebar isOpen={isOpen} toggle={toggle} />
+        </>
+    );
+};
+
+export default Home;
