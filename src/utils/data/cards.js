eac4213ea974b910f8ba09768ecb5c83722fc14b
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-05-15 23:48:50 +0200

    Add file with cards data

---
+++ src/utils/data/cards.js
@@ -0,0 +1,24 @@
+import Icon1 from '../images/Cards/Icon1.svg';
+import Icon2 from '../images/Cards/Icon2.svg';
+import Icon3 from '../images/Cards/Icon3.svg';
+
+export const cardsData = [
+    {
+        id: '1',
+        icon: Icon1,
+        header: 'Delicious',
+        paragraph: 'Choose your favorite smoothie'
+    },
+    {
+        id: '2',
+        icon: Icon2,
+        header: 'Health',
+        paragraph: 'Protect yourself and get your vitamins'
+    },
+    {
+        id: '3',
+        icon: Icon3,
+        header: 'Organic',
+        paragraph: 'All ingredients come from nature'
+    },
+]
