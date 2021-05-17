f0d2400a7be4aa399777ffec51e54b139888d950
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-05-17 15:02:26 +0200

    Refactor regex variable

--- src/utils/hooks/validateForm.js
+++ src/utils/hooks/validateForm.js
@@ -1,5 +1,7 @@
+const EMAIL_REG = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 export default function validateForm(values) {
-    const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
     let errors = {};
 
     if(!values.username.trim()) {
@@ -9,7 +11,7 @@ export default function validateForm(values) {
     if(!values.email) {
         errors.email = "Email is required";
 
-    } else if(!regex.test(String(values.email).toLowerCase())) {
+    } else if(!EMAIL_REG.test(String(values.email).toLowerCase())) {
         errors.email = "Email address is invalid";
     }
 
