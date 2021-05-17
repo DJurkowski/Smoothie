59dc4779a4914c1d4970288ec7a910a77f5473ce
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-05-17 14:24:42 +0200

    Add specialheader style element

--- src/components/CarouselSection/index.js
+++ src/components/CarouselSection/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Carousel from './Carousel';
-import { SectionContainer, SectionHeader, HeaderSpecial } from './SectionElements';
+import { SectionContainer, SectionHeader, HeaderSpecial, SectionWrapper } from './SectionElements';
 import { carouselSection as images } from '../../utils/data/sections';
 
 const CarouselSection = () => {
     return (
         <SectionContainer id="experience">
-            <SectionHeader>Every smoothie is an experience</SectionHeader>
-            <SectionHeader><HeaderSpecial>Make it count!</HeaderSpecial></SectionHeader>
-            <Carousel images={images} />
+            <SectionWrapper>
+                <SectionHeader>Every smoothie is an experience</SectionHeader>
+                <SectionHeader><HeaderSpecial>Make it count!</HeaderSpecial></SectionHeader>
+                <Carousel images={images} />
+            </SectionWrapper>
         </SectionContainer>
     )
 }
